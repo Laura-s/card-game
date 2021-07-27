aefea4435d824d0c27b2ff8d113a3e0b3a29bcca
Laura <laura.sfichi@yahoo.ro>
2021-07-27 16:09:21 +0300

    card game

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,168 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Poro card game</h1>
+  
+  <section class="game-board">
+    <Card
+      v-for="(card, index) in cardList"
+      :key="`card-${index}`"
+      :matched="card.matched"
+      :value="card.value"
+      :visible="card.visible"
+      :position="card.position"
+      @select-card="flipCard"
+    />
+  </section>
+  <h2>{{ status }}</h2>
+  <button @click="restartGame" class="restart-btn">
+     Restart Game
+  </button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import _ from "lodash";
+import { computed, ref, watch } from "vue";
+import Card from "./components/Card";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Card,
+  },
+  setup() {
+    const cardList = ref([]);
+    const userSelection = ref([]);
+    const status = computed(() => {
+      if (remainingPairs.value === 0) {
+        return "You won!";
+      } else {
+        return `Remaining Pairs: ${remainingPairs.value}`;
+      }
+    });
+    const remainingPairs = computed(() => {
+      const remainingCards = cardList.value.filter(
+        (card) => card.matched === false
+      ).length;
+      return remainingCards / 2;
+    });
+    const shuffleCards = () => {
+      cardList.value = _.shuffle(cardList.value);
+    };
+    const restartGame = () => {
+      shuffleCards();
+      cardList.value = cardList.value.map((card, index) => {
+        return {
+          ...card,
+          matched: false,
+          position: index,
+          visible: false,
+        };
+      });
+    };
+    const cardItems = [
+      "poro1",
+      "poro2",
+      "poro3",
+      "poro4",
+      "poro5",
+      "poro6",
+      "poro7",
+      "poro8",
+    ];
+    cardItems.forEach((item) => {
+      cardList.value.push({
+        value: item,
+        visible: false,
+        position: null,
+        matched: false,
+      });
+      cardList.value.push({
+        value: item,
+        visible: false,
+        position: null,
+        matched: false,
+      });
+    });
+    cardList.value = cardList.value.map((card, index) => {
+      return {
+        ...card,
+        position: index,
+      };
+    });
+    const flipCard = (payload) => {
+      cardList.value[payload.position].visible = true;
+      if (userSelection.value[0]) {
+        if (
+          userSelection.value[0].position === payload.position &&
+          userSelection.value[0].faceValue === payload.faceValue
+        ) {
+          return;
+        } else {
+          userSelection.value[1] = payload;
+        }
+      } else {
+        userSelection.value[0] = payload;
+      }
+    };
+    watch(
+      userSelection,
+      (currentValue) => {
+        if (currentValue.length === 2) {
+          const cardOne = currentValue[0];
+          const cardTwo = currentValue[1];
+          if (cardOne.faceValue === cardTwo.faceValue) {
+            cardList.value[cardOne.position].matched = true;
+            cardList.value[cardTwo.position].matched = true;
+          } else {
+            setTimeout(() => {
+              cardList.value[cardOne.position].visible = false;
+              cardList.value[cardTwo.position].visible = false;
+            }, 2000);
+          }
+          userSelection.value.length = 0;
+        }
+      },
+      { deep: true }
+    );
+    return {
+      cardList,
+      flipCard,
+      userSelection,
+      status,
+      shuffleCards,
+      restartGame,
+    };
+  },
+  mounted () {
+    this.restartGame()
   }
-}
+};
 </script>
 
 <style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.game-board {
+  display: grid;
+  grid-template-columns: repeat(4, 120px);
+  grid-template-rows: repeat(4, 120px);
+  grid-gap: 30px;
+  justify-content: center;
+}
+
+.restart-btn{
+  background-color: rgb(16, 38, 51);
+  color: goldenrod;
+  border: goldenrod 3px solid;
+  padding: 5px 15px;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="card" v-bind:class="{ flip: visible }" @click="selectCard">
+    <div  class="card-face is-front">
+      <img :src="`/images/${value}.png`" :alt="value" style="height: 120px; border-radius: 10px"  />
+    </div>
+    <div  class="card-face is-back"></div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    matched: {
+      type: Boolean,
+      default: false
+    },
+    position: {
+      type: Number,
+      required: true
+    },
+    value: {
+      type: String,
+      required: true
+    },
+    visible: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup(props, context) {
+    
+    const selectCard = () => {
+      context.emit('select-card', {
+        position: props.position,
+        faceValue: props.value
+      })
+    }
+    return {
+      selectCard
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .card {
+    position: relative;
+    transition: 0.5s transform ease-in;
+    transform-style: preserve-3d;
+  }
+  .card.flip{
+   transform: rotateY(180deg);
+ }
+  .card-face {
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    backface-visibility: hidden;
+  }
+
+  .card-face.is-front {
+    background-color: blueviolet;
+    color: white;
+    transform:rotateY(180deg);
+  }
+  .card-image {
+    max-width: 100%;
+  }
+  .card-face.is-back {
+    background-image: url('../../public/images/League.png');
+    background-repeat: no-repeat;
+    background-size: cover;
+    color: white;
+  }
+ 
+</style>
